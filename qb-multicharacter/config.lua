4d2874224a9f30cf740786bfe983cc50a3608ad6
KatFenzy <118754461+KatFenzy@users.noreply.github.com>
2023-03-16 12:17:05 +0700

    Add files via upload

---
+++ qb-multicharacter/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+Config.Interior = vector3(-814.89, 181.95, 76.85) -- Interior to load where characters are previewed
+Config.DefaultSpawn = vector3(-1035.71, -2731.87, 12.86) -- Default spawn coords if you have start apartments disabled
+Config.PedCoords = vector4(930.53, 46.94, 113.55, 247.5) -- Create preview ped at these coordinates
+Config.HiddenCoords = vector4(-812.23, 182.54, 76.74, 156.5) -- Hides your actual ped while you are in selection
+Config.CamCoords = vector4(933.95, 45.43, 113.55, 60.68) -- Camera coordinates for character preview screen
+Config.EnableDeleteButton = true -- Define if the player can delete the character or not
+Config.customNationality = false -- Defines if Nationality input is custom of blocked to the list of Countries
+
+Config.DefaultNumberOfCharacters = 3 -- Define maximum amount of default characters (maximum 5 characters defined by default)
+Config.PlayersNumberOfCharacters = { -- Define maximum amount of player characters by rockstar license (you can find this license in your server's database in the player table)
+    { license = "license:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", numberOfChars = 2 },
+}
